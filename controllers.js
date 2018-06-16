5d946bc70502267280b46a17e566a5bf68952b0f
Robert Corey <robertbcorey@gmail.com>
2018-06-16 10:33:13 -0400

    add nodemailer

--- controllers.js
+++ controllers.js
@@ -1,4 +1,23 @@
 let db = require('./db/db');
+let nodemailer = require('nodemailer');
+let emailpw = require('./passwords').email;
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: emailpw.address,
+    pass: emailpw.password
+  }
+});
+
+function createEmail (to, subject, html) {
+  return mailOptions = {
+    from: emailpw.address, // sender address
+    to: to, // list of receivers
+    subject: subject, // Subject line
+    html: '<p>' + html + '</p>'// plain text body
+  };
+}
 
 exports.createOrder = function (req, res) {
   //TODO backend validation goes here
@@ -6,9 +25,14 @@ exports.createOrder = function (req, res) {
     if (err) {
       res.sendStatus(400);
     } else {
-      res.sendStatus(200);
+      var emailBody = createEmail('robertbcorey@gmail.com', 'hellofromnewportbike', 'hi were sending you an email');
+      transporter.sendMail(emailBody, err =>{
+        if (err) {
+          res.sendStatus(400);
+        } else {
+          res.sendStatus(200);
+        }
+      });
     }
   });
 }
-
-exports.
